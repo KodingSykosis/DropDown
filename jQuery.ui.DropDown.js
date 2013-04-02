d55d1f701ec0cf5f6bf5dd1eddd795ab51a00915
Jack Godwin <godwhj@gmail.com>
2013-04-02 10:40:18 -0400

    Initial Import

--- jQuery.ui.DropDown.js
+++ jQuery.ui.DropDown.js
@@ -1,16 +1,36 @@
 ﻿(function ($) {
-    $.widget("ccic.DropDown", {
+    $.widget("kodingsykosis.DropDown", {
         options: {
             other: false,
             listSize: 10,
-            maxHeight: 350
+            maxHeight: 350,
+            emptyText: '-- Choose --',
+            ignoreEmptyOptions: true,
+            emptyOption: '^$|^0$'
         },
-        keyBlocks: [16],
+        keyBlocks: [16, 9],
 
         // Set up the widget
         _create: function () {
+            this.wrap = $('<div>');
+
             this.element
                 .addClass('ui-dropdown-source');
+
+            $.each(['float', 'position', 'width', 'height', 'top', 'left', 'right', 'bottom', 'display', 'line-height', 'font-size'],
+                $.proxy(function (idx, val) {
+                    this.wrap.css(val, this.element.css(val));
+                }, this));
+
+            this.element.hide();
+
+            var style = window.getComputedStyle(this.element[0]);
+            this.wrap
+                .css({
+                    width: style.width
+                });
+
+            this.element.wrap(this.wrap);
 
             //We are assuming the widget is instantiated on a select node
             //Wrap the element in a div or set it to display none?
@@ -20,7 +40,6 @@
             //Swap the select element out for our dropdown
             this.element
                 .removeAttr('name')
-                .hide()
                 .before(this.container);
 
             this.dropDownContainer = this._addDropDown()
@@ -47,11 +66,14 @@
 
             this.dropDownContainer
                 .hide();
+
+            if (this.options['rules'] && $.validator)
+                this._applyValidation(this.options['rules']);
         },
 
         /***********************************
-        **     Public methods
-        ***********************************/
+         **     Public methods
+         ***********************************/
 
         refresh: function () {
             var data = this._consumeSelect(this.element);
@@ -70,6 +92,14 @@
                 this.items = this.items.add(this.otherValue);
             }
 
+            if ($.validator) {
+                var rules = this.element.rules();
+                if (this._hasProperties(rules)) {
+                    this.value
+                        .rules(this.element.rules('remove'));
+                }
+            }
+
             this._computeContainerSize();
             this.selected(this.selected());
         },
@@ -86,8 +116,10 @@
                         .data('menu-item').selected = false;
                 }
 
-                if (item.length) {
+                if (item.length > 0) {
                     item = item.data('menu-item');
+                } else if (item.length == 0) {
+                    item = { value: '' };
                 }
 
                 this.list
@@ -104,24 +136,39 @@
                 this.value
                     .val(item.value || item.text);
 
-                this.element
-                    .val(item.value || item.text);
+                var empty = this.value.val() == '';
 
                 this.display
-                    .val(item.text);
+                    .val(empty ? this.options['emptyText'] : item.text)
+                    .toggleClass('ui-dropdown-display-empty', empty);
+
+                this._updScrollPosition(option);
             }
 
             return option;
         },
 
         expand: function () {
+            this.lastKeyisTab = false;
             this.dropDownContainer
                 .slideDown(120, $.proxy(this._computeContainerSize, this));
+
+            this._updScrollPosition();
         },
 
         collapse: function () {
+            this.lastKeyisTab = false;
+
             this.dropDownContainer
                 .slideUp(120);
+
+            this.items
+                .filter('.ui-state-hover')
+                .removeClass('ui-state-hover');
+
+            this.items
+                .filter('.ui-state-nomousy')
+                .removeClass('ui-state-nomousy');
         },
 
         toggle: function () {
@@ -131,17 +178,24 @@
         },
 
         /***********************************
-        **     Helpers
-        ***********************************/
+         **     Helpers
+         ***********************************/
 
         _consumeSelect: function (el) {
-            var data = [];
+            var data = [],
+                ignore = this.options['ignoreEmptyOptions'],
+                re = new RegExp(this.options['emptyOption'], 'i');
 
             el.children('option')
                 .each(function () {
                     var opt = $(this);
+
+                    if (re.test(opt.val()) && ignore) {
+                        return;
+                    }
+
                     data.push({
-                        text: opt.html(),
+                        text: opt.text(),
                         value: opt.val(),
                         selected: opt.is(':selected')
                     });
@@ -161,7 +215,7 @@
                 if (item.icon) {
                     el.append(
                         $('<span>').appendClass('ui-icon')
-                                   .appendClass(item.icon)
+                            .appendClass(item.icon)
                     );
                 }
 
@@ -185,8 +239,8 @@
             //just include the height of the visible items
             var listSize = this.options['listSize'];
             var height = this.list
-                    .children()
-                    .totalHeight(true, listSize);
+                .children()
+                .totalHeight(true, listSize);
 
             //Add the UL list margins & paddings
             height += this.list.outerHeight() -
@@ -216,18 +270,18 @@
             var btn = $('<div>', {
                 'class': 'ui-dropdown-trigger'
             }).css({
-                position: 'absolute',
-                top: '3px',
-                right: '3px',
-                bottom: '3px',
-                width: '16px',
-                borderRadius: '3px'
-            }).on({
-                click: $.proxy(this.toggle, this)
-            }).append(
-                $('<span>', { 'class': 'ui-icon ui-icon-triangle-1-s' })
-                    .css('margin-top', '1px')
-            );
+                    position: 'absolute',
+                    top: '3px',
+                    right: '3px',
+                    bottom: '3px',
+                    width: '16px',
+                    borderRadius: '3px'
+                }).on({
+                    click: $.proxy(this.toggle, this)
+                }).append(
+                    $('<span>', { 'class': 'ui-icon ui-icon-triangle-1-s' })
+                        .css('margin-top', '1px')
+                );
 
             return btn;
         },
@@ -241,8 +295,9 @@
 
             var node = $('<input>', {
                 type: 'text',
-                'class': 'ui-dropdown-othervalue',
-                'placeholder': 'Other'
+                'class': 'ui-dropdown-othervalue ignore-validation',
+                'placeholder': 'Other',
+                tabindex: -1
             });
 
             node.css({
@@ -259,6 +314,10 @@
                     selected: true
                 })
                 .appendTo(this.dropDownContainer);
+
+            if ($.validation) {
+                node.rules({ required: false });
+            }
 
             return node;
         },
@@ -284,6 +343,11 @@
                 'class': 'ui-dropdown-value'
             });
 
+            this.value
+                .on({
+                    change: $.proxy(this._onValueChanged, this)
+                });
+
             this.display = $('<input>', {
                 type: 'text',
                 readonly: 'readonly',
@@ -291,8 +355,15 @@
             });
 
             this.display
+                .css({
+                    width: '100%',
+                    boxSizing: 'border-box',
+                    height: '26px',
+                    margin: '0px'
+                })
                 .on({
                     focus: $.proxy(this.expand, this),
+                    blur: $.proxy(this._onLostFocus, this),
                     keydown: $.proxy(this._onKeydown, this)
                 });
 
@@ -302,7 +373,7 @@
         _addContainer: function () {
             return $('<div>', {
                 'class': 'ui-dropdown-container'
-            }).width(this.element.width());
+            }); //.width(this.element.width());
         },
 
         _addDropDown: function () {
@@ -313,7 +384,7 @@
             return el
                 .css({
                     width: this.container.outerWidth(),
-                    zIndex: 100
+                    zIndex: 1000
                 });
         },
 
@@ -327,20 +398,137 @@
             });
         },
 
+        _applyValidation: function (rules) {
+            this._injectElement();
+
+            this.value
+                .on({
+                    validationerror: $.proxy(this._onValidationError, this),
+                    validationsuccess: $.proxy(this._onValidationSuccess, this)
+                })
+                .rules(rules);
+        },
+
+        _hasProperties: function (object) {
+            for (var prop in object) {
+                if (object.hasOwnProperty(prop)) {
+                    return true;
+                }
+            }
+
+            return false;
+        },
+
+        _getFormValidator: function () {
+            return this.element
+                .parents('form')
+                .first()
+                .data('validator');
+        },
+
+        _injectElement: function () {
+            var validator = this._getFormValidator();
+            var builder = validator.elements;
+            var matches = builder.call(validator);
+            var myElement = this.value;
+
+            if (!matches.is(myElement)) {
+                //Inject our element
+                validator.elements = function () {
+                    var results = builder.call(validator).add(myElement);
+                    return results;
+                };
+            }
+        },
+
+        _updScrollPosition: function (node) {
+            var el = node || this.selected();
+            if (el.length == 0) {
+                this.scroll
+                    .tinyscrollbar_update('relative');
+
+                return;
+            }
+
+            var max = this.scroll.find('.content').height() - this.dropDownContainer.height();
+            var top = el.position().top - (this.dropDownContainer.height() / 2)
+                - (el.height() / 2);
+
+            top = top < 0 ? 0 : (top > max ? max : top);
+
+            if (max > this.options['maxHeight']) {
+                this.scroll
+                    .tinyscrollbar_update(top);
+            }
+        },
+
         /***********************************
-        **     Events
-        ***********************************/
+         **     Events
+         ***********************************/
+
+        _onValueChanged: function (event) {
+            var val = this.value.val();
+            var selected = this.list.find('li[value="' + val + '"]');
+
+            if (selected.length == 0 && this.otherValue) {
+                selected = this.otherValue;
+                this.otherValue.val(val);
+                var tmp = selected.data('menu-item');
+                tmp.text = val;
+            }
+
+            if (selected.length == 0) {
+                return;
+            }
+
+            var data = selected.data('menu-item');
+            this.selected(data);
+            this.collapse();
+        },
+
+        _onValidationError: function (event, data) {
+            var $el = $(event.target);
+
+            if ($el.is(this.value)) {
+                $el = this.display;
+            }
+
+            $el.addClass(data.errorClass).removeClass(data.validClass);
+            return false;
+        },
+
+        _onValidationSuccess: function (event, data) {
+            var $el = $(event.target);
+
+            if ($el.is(this.value)) {
+                $el = this.display;
+            }
+
+            $el.addClass(data.validClass).removeClass(data.errorClass);
+            return false;
+        },
 
         _onKeydown: function (event) {
+            this.lastKeyisTab = event.which == 9;
             if (this.keyBlocks.indexOf(event.which) > -1) return;
-
-            if (event.which != 9) //Allow default behavior for tab key
-                event.preventDefault();
-
+            /*
+             if (event.which == 9) { //Allow default behavior for tab key
+             this.lastKeyisTab = true;
+             return;
+             }
+             */
+            event.preventDefault();
             $.debug('debug', this.widgetName, 'Keypress', event.which);
             var open = this.list.is(':visible');
-            var opt = open ? this.items.filter('.ui-state-hover') : this.selected();
+            var opt = this.items.filter('.ui-state-hover');
             var max = this.items.length;
+            //var nomousy = false;
+
+            if (opt.length == 0) {
+                opt = this.selected();
+            }
+
+            //this.lastKeyisTab = false;
 
             switch (event.which) {
                 case 27:
@@ -353,6 +541,7 @@
                     } else {
                         opt = this.items.last();
                     }
+                    //nomousy = true;
 
                     break;
                 case 40: //Down
@@ -362,12 +551,17 @@
                         opt = this.items.first();
                     }
 
+                    //nomousy = true;
+
                     break;
-                case 9: //Tab
+                //case 9: //Tab      
                 case 32: //Space
                 case 13: //Enter
                     if (open) {
-                        this.selected(opt);
+                        if (opt.length > 0 && opt.val() != '') {
+                            this.selected(opt);
+                        }
+
                         this.collapse();
                         return;
                     }
@@ -381,7 +575,11 @@
             this.items
                 .filter('.ui-state-hover')
                 .removeClass('ui-state-hover');
-
+            /*
+             this.items
+             .filter('.ui-state-nomousy')
+             .removeClass('ui-state-nomousy');
+             */
             if (!open) {
                 this.expand();
             }
@@ -394,6 +592,12 @@
             }
 
             opt.addClass('ui-state-hover');
+            /*
+             if (nomousy) {
+             opt.addClass('ui-state-nomousy');
+             }
+             */
+            this._updScrollPosition(opt);
         },
 
         _onOtherKeydown: function (event) {
@@ -425,6 +629,36 @@
 
             this.selected(item);
             this.collapse();
+        },
+
+        _onLostFocus: function (event) {
+            var opt = this.items.filter('.ui-state-hover');
+            if (opt.length > 0 && opt.val() != ''/* && this.lastKeyisTab*/) {
+                this.selected(opt);
+            }
+
+            this.collapse();
         }
     });
+
+    if ($.validator) {
+        var highlight = $.validator.defaults.highlight;
+        var unhighlight = $.validator.defaults.unhighlight;
+
+        $.extend(true, $.validator.defaults, {
+            ignore: $.validator.defaults.ignore + ',.ignore-validation',
+            highlight: function (element, errorClass, validClass) {
+                var data = { errorClass: errorClass, validClass: validClass };
+                if ($(element).trigger('validationerror', data) != false) {
+                    highlight(element, errorClass, validClass);
+                }
+            },
+            unhighlight: function (element, errorClass, validClass) {
+                var data = { errorClass: errorClass, validClass: validClass };
+                if ($(element).trigger('validationsuccess', data) != false) {
+                    unhighlight(element, errorClass, validClass);
+                }
+            }
+        });
+    }
 })(jQuery);
